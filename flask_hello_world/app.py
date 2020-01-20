e547ebb4198f293c75fbf5b4e724a2810b132778
Roman Imankulov <roman.imankulov@gmail.com>
2020-01-20 07:19:46 +0000

    Make the app return JSON response with more info

--- flask_hello_world/app.py
+++ flask_hello_world/app.py
@@ -1,8 +1,21 @@
-from flask import Flask
+import os
+import socket
+
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
+HELLO_WORLD_MESSAGE = os.getenv("HELLO_WORLD_MESSAGE", "Hello, world")
+
+
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return jsonify(
+        {
+            "message": HELLO_WORLD_MESSAGE,
+            "server_hostname": socket.gethostname(),
+            "remote_addr": request.remote_addr,
+            "http_host": request.host,
+        }
+    )
